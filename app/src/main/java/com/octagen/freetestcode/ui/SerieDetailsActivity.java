00e3e8cfd5bb223f4e3d28dd74954dda4e6a742c
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-07-03 11:17:07 +0000

    SerieDetailsActivity: Set the font of the correction & validation buttons.

--- app/src/main/java/com/octagen/freetestcode/ui/SerieDetailsActivity.java
+++ app/src/main/java/com/octagen/freetestcode/ui/SerieDetailsActivity.java
@@ -2,14 +2,28 @@ package com.octagen.freetestcode.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
+import com.octagen.customviews.TypeFaces;
 import com.octagen.freetestcode.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class SerieDetailsActivity extends AppCompatActivity {
+
+    @Bind(R.id.correctionBtn)
+    Button correctionBtn;
+    @Bind(R.id.validationBtn)
+    Button validationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_serie_details);
+        ButterKnife.bind(this);
+
+        correctionBtn.setTypeface(TypeFaces.getTypeFace(this, "fonts/cent.TTF"));
+        validationBtn.setTypeface(TypeFaces.getTypeFace(this, "fonts/cent.TTF"));
     }
 }
