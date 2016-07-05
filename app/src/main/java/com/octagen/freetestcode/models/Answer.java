da67b5e1b29e243036c7bf6ee9111c14b42315f9
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-07-05 11:32:20 +0000

    Create the Answer class.

---
+++ app/src/main/java/com/octagen/freetestcode/models/Answer.java
@@ -0,0 +1,49 @@
+package com.octagen.freetestcode.models;
+
+/**
+ * Created by Mohammed Aouf ZOUAG on 05/07/2016.
+ */
+public class Answer {
+    private int id;
+    private String text;
+    private boolean isValid;
+
+    public Answer(int id, String text, boolean isValid) {
+        this.id = id;
+        this.text = text;
+        this.isValid = isValid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setValid(boolean valid) {
+        isValid = valid;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "isValid=" + isValid +
+                ", text='" + text + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
