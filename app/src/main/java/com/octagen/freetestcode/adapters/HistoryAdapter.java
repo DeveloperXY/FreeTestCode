859faf02b06bf4e5676404b6d3f3334b7f020708
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-08-04 09:21:26 +0100

    HistoryAdapter: write the code of the HistoryViewHolder.

--- app/src/main/java/com/octagen/freetestcode/adapters/HistoryAdapter.java
+++ app/src/main/java/com/octagen/freetestcode/adapters/HistoryAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.octagen.freetestcode.R;
 import com.octagen.freetestcode.adapters.base.BaseSearchAdapter;
@@ -11,6 +13,9 @@ import com.octagen.freetestcode.adapters.base.BinderViewHolder;
 import com.octagen.freetestcode.models.HistoryItem;
 
 import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Mohammed Aouf ZOUAG on 03/08/2016.
@@ -34,13 +39,32 @@ public class HistoryAdapter extends BaseSearchAdapter<HistoryAdapter.HistoryView
 
     public class HistoryViewHolder extends BinderViewHolder<HistoryItem> {
 
+        @Bind(R.id.historyImg)
+        ImageView historyImg;
+        @Bind(R.id.timeLabel)
+        TextView timeLabel;
+        @Bind(R.id.dateLabel)
+        TextView dateLabel;
+        @Bind(R.id.categoryLabel)
+        TextView categoryLabel;
+        @Bind(R.id.serieLabel)
+        TextView serieLabel;
+        @Bind(R.id.ratioLabel)
+        TextView ratioLabel;
+
         public HistoryViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
 
         @Override
         public void bind(HistoryItem item) {
-
+            historyImg.setImageResource(item.getImage());
+            timeLabel.setText(item.getTime());
+            dateLabel.setText(item.getDate());
+            categoryLabel.setText(item.getCategory());
+            serieLabel.setText(item.getSerie());
+            ratioLabel.setText(item.getRatio());
         }
     }
 }
