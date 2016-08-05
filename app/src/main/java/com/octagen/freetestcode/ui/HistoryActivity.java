2b2e04da5a6e14c6519c715bb7c3e038a2ff525d
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-08-05 09:13:22 +0100

    HistoryActivity: setup & populate the RecyclerView with data.

--- app/src/main/java/com/octagen/freetestcode/ui/HistoryActivity.java
+++ app/src/main/java/com/octagen/freetestcode/ui/HistoryActivity.java
@@ -1,20 +1,29 @@
 package com.octagen.freetestcode.ui;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.octagen.freetestcode.R;
+import com.octagen.freetestcode.adapters.HistoryAdapter;
 import com.octagen.freetestcode.models.HistoryItem;
 import com.octagen.freetestcode.utils.DatabaseAdapter;
+import com.octagen.freetestcode.utils.decorations.SpacesItemDecoration;
 
 import java.util.List;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class HistoryActivity extends ActionbarActivity {
 
+    @Bind(R.id.historyRecyclerView)
+    RecyclerView historyRecyclerView;
+
     private List<HistoryItem> mHistoryItems;
     private DatabaseAdapter mDatabaseAdapter;
+    private HistoryAdapter mHistoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +31,25 @@ public class HistoryActivity extends ActionbarActivity {
         setContentView(R.layout.activity_history);
         ButterKnife.bind(this);
 
-        setupActionBar();
-
         mDatabaseAdapter = new DatabaseAdapter(this);
+
+        setupActionBar();
+        setupRecyclerView();
+        populateRecyclerView();
+    }
+
+    private void setupRecyclerView() {
         mHistoryItems = mDatabaseAdapter.getAllHistoryRecords();
         Log.i("HISTORY", mHistoryItems + "");
+        historyRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        historyRecyclerView.addItemDecoration(new SpacesItemDecoration(8));
+    }
+
+    private void populateRecyclerView() {
+        if (mHistoryAdapter == null) {
+            mHistoryAdapter = new HistoryAdapter(this, mHistoryItems);
+            historyRecyclerView.setAdapter(mHistoryAdapter);
+        } else
+            mHistoryAdapter.animateTo(mHistoryItems);
     }
 }
