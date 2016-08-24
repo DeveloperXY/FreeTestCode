197d9ffa747ef4745bbd7ca1e853cdb8e982beaf
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-08-24 18:42:24 +0100

    Modify the PanelItem class properties.

--- app/src/main/java/com/octagen/freetestcode/models/PanelItem.java
+++ app/src/main/java/com/octagen/freetestcode/models/PanelItem.java
@@ -4,24 +4,79 @@ package com.octagen.freetestcode.models;
  * Created by Mohammed Aouf ZOUAG on 18/08/2016.
  */
 public class PanelItem {
-    private int image;
+    private int id;
+    private String arabic;
+    private String french;
+    private String category;
+    private String image;
+    private String audio;
 
-    public PanelItem(int image) {
+    public PanelItem(int id, String arabic, String french, String category, String image, String audio) {
+        this.id = id;
+        this.arabic = arabic;
+        this.french = french;
+        this.category = category;
         this.image = image;
+        this.audio = audio;
     }
 
-    public int getImage() {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getArabic() {
+        return arabic;
+    }
+
+    public void setArabic(String arabic) {
+        this.arabic = arabic;
+    }
+
+    public String getFrench() {
+        return french;
+    }
+
+    public void setFrench(String french) {
+        this.french = french;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImage() {
         return image;
     }
 
-    public void setImage(int image) {
+    public void setImage(String image) {
         this.image = image;
+    }
+
+    public String getAudio() {
+        return audio;
+    }
+
+    public void setAudio(String audio) {
+        this.audio = audio;
     }
 
     @Override
     public String toString() {
         return "PanelItem{" +
-                "image=" + image +
+                "id=" + id +
+                ", arabic='" + arabic + '\'' +
+                ", french='" + french + '\'' +
+                ", category='" + category + '\'' +
+                ", image='" + image + '\'' +
+                ", audio='" + audio + '\'' +
                 '}';
     }
 }
